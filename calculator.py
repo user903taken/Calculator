067e7eb0a2254b8808ba6c4226193a77603d74cc
mikecancode <mikemasaku17@gmail.com>
2025-09-22 21:45:00 +0300

    Add calculator.py

---
+++ calculator.py
@@ -0,0 +1,34 @@
+print("Basic calculator 🧮")
+history = []
+while True:
+    num1=int(input("enter a number"))
+    num2=int(input("enter a number")) 
+    operator=input("enter an operator:(+,-,/,*,**,//,%) or 'q' to quit ") 
+    if operator == "+": 
+        result = num1+num2 
+    elif operator == "-":
+        result = num1-num2 
+    elif operator=="/": 
+        result = num1/num2
+    elif operator == "*": 
+        result = num1*num2
+    elif operator == "**":
+        result = num1**num2
+    elif operator == "//": 
+        result = num1//num2
+    elif operator == "%":
+        result  = num1%num2
+    elif operator == "q":
+        print("exiting program")
+        break
+    else: 
+        print("Invalid operator")
+
+    print(result)
+    history.append(f"{num1} {operator} {num2} = {result}")
+    
+    
+print("==Calculation History==")
+for h in history:
+    print(h)
+    
